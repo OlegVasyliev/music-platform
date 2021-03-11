4ea539c150b74e18f62bc50419dcdacbc8777a3a
Oleh Vasiliev <olegvasiliev1991@gmail.com>
2021-03-11 22:48:50 +0200

    add navbar, main layout, pages

---
+++ client/pages/tracks/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MainLayout from '../../layouts/MainLayout';
+
+const Index = () => {
+	return (
+		<MainLayout>
+			Track list
+		</MainLayout>
+	);
+};
+export default Index;
