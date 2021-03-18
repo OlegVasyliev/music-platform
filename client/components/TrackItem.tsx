b99603ebcb3e58b5176e87239109a2d409bbb785
Oleh Vasiliev <olegvasiliev1991@gmail.com>
2021-03-18 18:44:56 +0200

    add styles, sass, styled tracklist

--- client/components/TrackItem.tsx
+++ client/components/TrackItem.tsx
@@ -1,16 +1,33 @@
+import { Card, Grid, IconButton } from '@material-ui/core';
 import React from 'react';
 import { ITrack } from '../types/track';
+import styles from '../styles/TrackItem.module.scss';
+import { Delete, Pause, PlayArrow } from '@material-ui/icons';
 
-interface TrackItemsProps {
+interface TrackItemProps {
 	track: ITrack;
 	active?: boolean;
 }
 
-const  TrackItem: React.FC<TrackItemsProps> = ({track, active = false}) => {
+const  TrackItem: React.FC<TrackItemProps> = ({track, active = false}) => {
 	return (
-		<div>
-			{track.name}
-		</div>
+		<Card className={styles.track}>
+			<IconButton>
+				{active
+					? <Pause/>
+					:<PlayArrow/>
+				}
+			</IconButton>
+			<img className={styles.img} src={track.picture}/>
+			<Grid container direction="column" className={styles.grid}>
+				<div>{track.name}</div>
+				<div style={{fontSize: 12, color: 'gray'}}>{track.artist}</div>
+			</Grid>
+			{active && <div> 02:42 / 03:32</div>}
+			<IconButton className={styles.icon}>
+				<Delete/>
+			</IconButton>
+		</Card>
 	);
 };
 
