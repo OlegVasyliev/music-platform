73be2bc001629ba49c752d8a5f183a6023f8f41c
Oleh Vasiliev <olegvasiliev1991@gmail.com>
2021-03-23 20:03:34 +0200

    add StepWrapper, create track page

--- client/pages/tracks/create.tsx
+++ client/pages/tracks/create.tsx
@@ -1,13 +1,67 @@
-import React, {useState} from 'react';
-import MainLayout from '../../layouts/MainLayout';
-
+import { Button, Grid, TextField } from "@material-ui/core";
+import React, { useState } from "react";
+import FileUpload from "../../components/FileUpload";
+import StepWrapper from "../../components/StepWrapper";
+import MainLayout from "../../layouts/MainLayout";
 
 const Create = () => {
-   return (
-	   <MainLayout>
-		   	<h1>Download track</h1>
-	   </MainLayout>
-   );
+  	const [activeStep, setActiveStep] = useState(0);
+ 	const [picture, setPicture] = useState(null)
+	const [audio, setAudio] = useState(null)
+
+  const next = () => {
+    if (activeStep !== 2) {
+      setActiveStep((prev) => prev + 1);
+    }
+  };
+
+  const back = () => {
+    setActiveStep((prev) => prev - 1);
+  };
+
+  return (
+    <MainLayout>
+      <StepWrapper activeStep={activeStep}>
+        {activeStep === 0 && (
+          <Grid container direction={"column"} style={{ padding: 20 }}>
+            <TextField
+             
+              style={{ marginTop: 10 }}
+              label={"Track name"}
+            />
+            <TextField
+              
+              style={{ marginTop: 10 }}
+              label={"Singer name"}
+            />
+            <TextField
+              
+              style={{ marginTop: 10 }}
+              label={"Lyrics"}
+              multiline
+              rows={3}
+            />
+          </Grid>
+        )}
+        {activeStep === 1 && (
+          <FileUpload setFile={setPicture} accept="image/*">
+            <Button>Download picture</Button>
+          </FileUpload>
+        )}
+        {activeStep === 2 && (
+          <FileUpload setFile={setAudio} accept="audio/*">
+            <Button>Download audio</Button>
+          </FileUpload>
+        )}
+      </StepWrapper>
+      <Grid container justifyContent="space-between">
+        <Button disabled={activeStep === 0} onClick={back}>
+          Back
+        </Button>
+        <Button onClick={next}>Next</Button>
+      </Grid>
+    </MainLayout>
+  );
 };
 
 export default Create;
